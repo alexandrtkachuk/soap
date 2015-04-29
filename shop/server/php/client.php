be79dd5e79aa46511d12e36e0450828ba002dd1e
aitkachuk home <you@example.com>
2015-04-29 13:38:00 +0300

    dfghj

--- shop/server/php/client.php
+++ shop/server/php/client.php
@@ -1,5 +1,8 @@
-<?php  
-  $client = new SoapClient("http://192.168.0.15/~user7/soap/shop/server/php/test.php");  
+<?php 
+ini_set('soap.wsdl_cache_enabled', '0');
+ini_set('soap.wsdl_cache_ttl', '0'); 
+
+  $client = new SoapClient("http://192.168.56.88/soap/shop/server/php/test.php");  
     print_r($client->__getFunctions() );
-    #print($client->test("ibm"));  
+    print($client->test("ibm"));  
 ?>
--- shop/server/php/index.php
+++ shop/server/php/index.php
@@ -16,9 +16,14 @@ function test($test)
    return  $var;        
 }  
 
+ini_set('soap.wsdl_cache_enabled', '0');
+ini_set('soap.wsdl_cache_ttl', '0'); 
 
 ini_set("soap.wsdl_cache_enabled", "0"); // отключаем кэширование WSDL 
-$server = new SoapServer('http://192.168.0.15/~user7/soap/shop/server/php/test.php' );  
+ini_set("soap.wsdl_cache_dir", '/home/alexandr/soap/shop/server/php/tmp/'); // где будет кэш
+#ini_set("soap.wsdl_cache_ttl", "2");
+
+$server = new SoapServer('http://192.168.56.88/soap/shop/server/php/test.php' );  
 $server->addFunction("getQuote");  
 $server->addFunction("test");
 $server->handle();  
